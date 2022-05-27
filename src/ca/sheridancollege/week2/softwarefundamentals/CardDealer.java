d30ba41c223c3334da7ea7cb3ddb60384cef9b61
mina-p1 <105944850+mina-p1@users.noreply.github.com>
2022-05-27 15:19:22 -0400

    Update CardDealer.java

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -15,5 +15,7 @@ public class CardDealer
         Card nineClubs = new Card("Clbs", 9);
         nineClubs.setSuit("diamonds");
         Card jackSpade = new Card ("Diamonds", 11);
+        System.out.println(getSuit);
+        System.out.println(getValue);
     }
 }

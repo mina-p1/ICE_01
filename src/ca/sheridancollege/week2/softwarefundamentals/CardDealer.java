06e0c99ef9594a5f6510bb9df06750a43f15a193
peter mina <minap@sheridanc.on.ca>
2022-05-27 15:15:47 -0400

    added new Card Object

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Peter Mina
+ * 991670171
  */
 package ca.sheridancollege.week2.softwarefundamentals;
 
@@ -15,5 +14,6 @@ public class CardDealer
     {
         Card nineClubs = new Card("Clbs", 9);
         nineClubs.setSuit("diamonds");
+        Card jackSpade = new Card ("Diamonds", 11);
     }
 }
